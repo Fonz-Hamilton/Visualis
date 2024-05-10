0db9d78e677557f3bc7373a04aa77e7e57a21b22
Fonz <fonz.hamilton@gmail.com>
2024-05-09 22:25:57 -0700

    Grouped Junit tests by module
    Made a Junit class for repository tests and one for service tests. Will
    separate all future tests into like tests

--- src/test/java/org/fonzhamilton/visualis/ServiceTests.java
+++ src/test/java/org/fonzhamilton/visualis/ServiceTests.java
@@ -9,14 +9,10 @@ import org.fonzhamilton.visualis.model.Role;
 import org.fonzhamilton.visualis.model.User;
 import org.fonzhamilton.visualis.repository.DataInfoRepository;
 import org.fonzhamilton.visualis.repository.DataRepository;
-import org.fonzhamilton.visualis.repository.RoleRepository;
 import org.fonzhamilton.visualis.repository.UserRepository;
-import org.fonzhamilton.visualis.security.UserPrincipal;
 import org.fonzhamilton.visualis.service.DataInfoService;
 import org.fonzhamilton.visualis.service.DataService;
 import org.fonzhamilton.visualis.service.RoleService;
-import org.fonzhamilton.visualis.service.UserService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -26,7 +22,6 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -35,12 +30,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 @SpringBootTest
 
-class VisualisApplicationTests {
+class ServiceTests {
 
     @Autowired
     DataInfoRepository dataInfoRepository;
-    @Autowired
-    RoleRepository roleRepository;
     @Autowired
     UserRepository userRepository;
     @Autowired
@@ -51,64 +44,6 @@ class VisualisApplicationTests {
     DataInfoService dataInfoService;
     @Autowired
     RoleService roleService;
-    @Autowired
-    UserService userService;
-
-
-    @BeforeAll
-    public static void initializeStuff() {
-
-
-    }
-
-    @Test
-    public void testDataInfoRepositoryExistsByName() {
-
-        String name = "testName";
-
-        DataInfo dataInfo = new DataInfo();
-        dataInfo.setName(name);
-        dataInfoRepository.save(dataInfo);
-        boolean exists = dataInfoRepository.existsByName(name);
-
-        assertTrue(exists, "Data of name should exist");
-
-    }
-
-    @Test
-    public void testFindDataByName() {
-        String nameToSearch = "YourDataName";
-        Data testData = new Data();
-        testData.setName(nameToSearch);
-        dataRepository.save(testData);
-
-
-        Data foundData = dataRepository.findDataByName(nameToSearch);
-
-        assertNotNull(foundData, "Data should be found");
-        assertEquals(nameToSearch, foundData.getName(), "Names should match");
-
-    }
-
-    @Test
-    public void testFindDataById() {
-
-        Data testData = new Data();
-        testData.setId(1L); // setting the id as a long for testing
-                                // Future me asks why wouldnt it be a long?
-        testData.setName("TestName");
-        dataRepository.save(testData);
-
-
-        List<Data> foundDataList = dataRepository.findDataById(1L);
-
-
-        assertFalse(foundDataList.isEmpty(), "Data should be found");
-        assertEquals(1, foundDataList.size(), "Only one data entry should be found");
-        assertEquals("TestName", foundDataList.get(0).getName(), "Names should match");
-
-    }
-
 
     @ParameterizedTest
     @MethodSource("userIdProvider")
@@ -150,7 +85,6 @@ class VisualisApplicationTests {
         List<Data> foundDataList = dataRepository.getAllDataByUserId(userId);
 
         assertEquals(2, foundDataList.size(), "Two data entries should be found");
-
     }
 
     // Helper method
@@ -163,106 +97,9 @@ class VisualisApplicationTests {
     }
 
     @Test
-    public void testFindRoleByName() {
-
-
-        String roleName = "test";
-        Role testRole = new Role();
-        testRole.setName(roleName);
-        roleRepository.save(testRole);
-
-        Role foundRole = roleRepository.findRoleByName(roleName);
-
-        assertNotNull(foundRole, "Role is found");
-        assertEquals(roleName, foundRole.getName(), "Role names should match");
-
-
-    }
-
-
-    @Test
-    public void testFindRoleByUser() {
-
-        // id 1 should be admin
-        long roleId = 1L;
-        // roles are list in case users with multiple roles are needed
-        List<Role> foundRole = roleRepository.findRoleByUser(roleId);
-
-        assertEquals(foundRole.get(0).getName(), "ROLE_ADMIN", "Role should match");
-
-    }
-
-
-    @Test
-    public void testFindUserByEmail() {
-
-        String email = "testFind@example.com";
-        User testUser = new User();
-        testUser.setEmail(email);
-        userRepository.save(testUser);
-
-
-        User foundUser = userRepository.findUserByEmail(email);
-
-        assertNotNull(foundUser, "User should be found");
-        assertEquals(email, foundUser.getEmail(), "User emails should match");
-
-    }
-
-
-    @Test
-    public void testFindUserByUserName() {
-
-        String userName = "testUser";
-        User testUser = new User();
-        testUser.setUserName(userName);
-        userRepository.save(testUser);
-
-
-        User foundUser = userRepository.findUserByUserName(userName);
-
-        assertNotNull(foundUser, "User should be found");
-        assertEquals(userName, foundUser.getUserName(), "User names are supposed to match!!");
-
-    }
-
-
-    @Test
-    public void testUserExistsByEmail() {
-
-        String email = "test@example.com";
-        User testUser = new User();
-        testUser.setEmail(email);
-        userRepository.save(testUser);
-
-        boolean exists = userRepository.existsByEmail(email);
-
-        assertTrue(exists, "User should exist with that email");
-
-    }
-
-
-    @Test
-    public void testExistsByUserName() {
-
-        String existingEmail = "test2@example.com";
-        User testUser = new User();
-        testUser.setEmail(existingEmail);
-        userRepository.save(testUser);
-
-
-        boolean exists = userRepository.existsByEmail(existingEmail);
-
-
-        assertTrue(exists, "User should exist with that email");
-
-    }
-
-    @Test
     public void testDataInfoServiceExistsByName() {
 
         String name = "nonexistentName";
-
         boolean exists = dataInfoService.existsByName(name);
 
         assertFalse(exists, "Data should not exist with the specified name");
@@ -275,9 +112,7 @@ class VisualisApplicationTests {
         exists = dataInfoService.existsByName(name);
 
         assertTrue(exists, "Data should exist by that name");
-
     }
-
 
     @Test
     public void testDataServiceGetDataByName() {
@@ -292,20 +127,15 @@ class VisualisApplicationTests {
 
         assertNotNull(result, "Data should be found with the specified name");
         assertEquals(existingName, result.getName(), "Data names should match");
-
-
     }
 
     @Test
     public void testRoleServiceFindRoleByRoleName() {
 
         String roleName = "ROLE_ADMIN";
-
         Role result = roleService.findRoleByRoleName(roleName);
 
         assertNotNull(result, "Role should be found with the specified name");
         assertEquals(roleName, result.getName(), "Role names should match");
-
     }
-
 }
